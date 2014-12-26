84287ba1261b38f2ab71ae58de0abf5aa09ee43f
Aboli Kumthekar <abolikumthekar@gmail.com>
2014-12-25 20:04:30 -0500

    basic express application, run with npm start

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ This is the QuickHitch application created with NodeJS, AngularJS, and MongoDB.
 
 Initial application created using the following tutorial:
 http://blog.ijasoneverett.com/2013/03/a-sample-app-with-node-js-express-and-mongodb-part-1/
+
+Additionally helpful:
+http://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express = require('express'); 
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var favicon = require('serve-favicon'); //icon
+var logger = require('morgan'); // log requests to the console (express4)
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
+var mongoose = require('mongoose'); // mongoose for mongodb
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express(); // create our app w/ express
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +17,11 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+
+//mongoose.connect('mongodb://localhost/test'); 
+
+//Configuration:
+//using express modules to add more functionality to our application.
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +29,7 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routing information through express
 app.use('/', routes);
 app.use('/users', users);
 
@@ -56,6 +63,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
